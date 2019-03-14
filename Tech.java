3a905e864ef9c2dcd55628b1363070e290c3c016
yukari6666 <yukari6666@naver.com>
2019-03-14 10:09:14 +0900

    .....

---
+++ Tech.java
@@ -0,0 +1,11 @@
+
+public class Tech {
+	public static void main(String[] args) {
+		char a = 'A';
+		char b = 'B';
+		System.out.print('\"');
+		System.out.print(a);
+		System.out.print(b);
+		System.out.print('\"');
+	}
+}
