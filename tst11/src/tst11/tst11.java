c7811cc22e4b31d9038533e7fbde240d2c89dc2e
yukari6666 <yukari6666@naver.com>
2019-03-22 18:31:31 +0900

    ...

---
+++ tst11/src/tst11/tst11.java
@@ -0,0 +1,39 @@
+package tst11;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class tst11 {
+	public static void main(String[] args) {
+		int j;
+		for (j = 1; j <= 4000000; j++) {
+			Random r = new Random();
+			Scanner scan = new Scanner(System.in);
+			
+			int i = 0;
+			int userChoice = r.nextInt(6) + 1;
+			int comChoice = r.nextInt(6) + 1;
+			
+			System.out.println("주사위를 굴릴까요? (종료는 q를 눌러주세요)");
+			String dice = scan.nextLine();
+			if (dice.equalsIgnoreCase("q")) {
+				System.out.println("게임이 종료되었습니다.");
+				break;
+			}
+			System.out.println("컴퓨터의 주사위는: " + comChoice + "입니다.");
+			
+			if (userChoice == comChoice) {
+				System.out.println("당신의 주사위는: "+ userChoice + "입니다. ");
+				System.out.println("무승부");
+			} else if (userChoice > comChoice) {
+				System.out.println("당신의 주사위는: "+ userChoice + "입니다. ");
+				System.out.println("승리");
+			} else if (comChoice > userChoice) {
+				System.out.println("당신의 주사위는: "+ userChoice +"입니다. " );
+				System.out.println( "패배");
+			}else {
+				System.out.println("제대로 실행되지 못 했습니다.");
+			}
+		}
+	}
+}
